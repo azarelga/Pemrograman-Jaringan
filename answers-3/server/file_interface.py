1e3fd898f711a52a5ece64f7701fb671024775e4
azarelga <skootydack@gmail.com>
2025-05-12 15:37:21 +0700

    feat: answers for assignment-3

---
+++ answers-3/server/file_interface.py
@@ -0,0 +1,63 @@
+import os
+import json
+import base64
+from glob import glob
+
+
+class FileInterface:
+    def __init__(self):
+        os.chdir("files/")
+
+    def list(self, params=[]):
+        try:
+            filelist = glob("*.*")
+            return dict(status="OK", data=filelist)
+        except Exception as e:
+            return dict(status="ERROR", data=str(e))
+
+    def get(self, params=[]):
+        try:
+            filename = params[0]
+            if filename == "":
+                return None
+            fp = open(f"{filename}", "rb")
+            isifile = base64.b64encode(fp.read()).decode()
+            return dict(status="OK", data_namafile=filename, data_file=isifile)
+        except Exception as e:
+            return dict(status="ERROR", data=str(e))
+
+    def upload(self,params=[]):
+        try:
+            filename = params[0]
+            filecontent = params[1]
+
+            # cek filenya kosong apa tidak
+            if (filecontent == ''):
+                return dict(status="ERROR", data="file yang diupload kosong!")
+
+            # menulis ke dalam file
+            isifile = base64.b64decode(filecontent)
+            fp = open(filename,'wb+')
+            fp.write(isifile)
+            fp.close()
+            
+            return dict(status="OK", data_namafile=filename)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def delete(self, params=[]):
+        try:
+            filename = params[0]
+            if os.path.exists(filename):
+                os.remove(filename)
+                return dict(status="OK", data_namafile=filename)
+            else:
+                return dict(status="ERROR", data="file tidak ditemukan!")
+        except Exception as e:
+            return dict(status="ERROR", data=str(e))
+
+
+if __name__ == "__main__":
+    f = FileInterface()
+    print(f.list())
+    print(f.get(["pokijan.jpg"]))
