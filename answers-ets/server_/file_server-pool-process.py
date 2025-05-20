9bdbd68cdf836884b14ef43af6cd4f6263fcf3d6
azarelga <skootydack@gmail.com>
2025-05-20 15:44:53 +0700

    feat: draft answers

---
+++ answers-ets/server_/file_server-pool-process.py
@@ -0,0 +1,69 @@
+import socket
+import logging
+from concurrent.futures import ProcessPoolExecutor
+from file_protocol import FileProtocol
+import sys
+
+fp = FileProtocol()
+
+
+def ProcessTheClient(connection):
+    try:
+        # request
+        data = connection.recv(1024)
+        request = data.decode().split(" ")[0]
+
+        if request == "UPLOAD":
+            # Use a larger buffer size for better performance
+            filename = data.decode().split(" ")[1]
+            buffer = data.decode().split(" ")[2].encode()
+            total_received = len(buffer)
+            while True:
+                if b"\r\n\r\n" in buffer:
+                    break
+                data = connection.recv(65536)  # Increased from 32 bytes
+                if not data:
+                    break
+                buffer += data
+                total_received += len(data)
+            print()
+
+            if buffer:
+                hasil = fp.proses_string(request, filename, buffer.decode())
+        else:
+            hasil = fp.proses_string(data.decode())
+
+        hasil = hasil + "\r\n\r\n"
+        connection.sendall(hasil.encode())
+    except Exception as e:
+        logging.warning(f"error during client processing: {e}")
+    finally:
+        connection.close()
+    return
+
+
+def Server(ipaddress="0.0.0.0", port=8885, max_workers=20):
+    ipinfo = (ipaddress, port)
+    the_clients = []
+
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.bind(ipinfo)
+    my_socket.listen(1)
+
+    logging.warning(f"server berjalan di ip address {ipinfo}")
+    with ProcessPoolExecutor(max_workers) as executor:
+        while True:
+            connection, client_address = my_socket.accept()
+            logging.warning("connection from {}".format(client_address))
+            p = executor.submit(ProcessTheClient, connection)
+            the_clients.append(p)
+
+
+def main():
+    worker = int(input("num workers: "))
+    Server(ipaddress="0.0.0.0", port=6969, max_workers=worker)
+
+
+if __name__ == "__main__":
+    main()
